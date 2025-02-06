6a5e8a45a0967a8a7966fbb21557fcf49990cce8
Saikirangoud Palcham <saikiranpalcham09@gmail.com>
2025-02-06 06:50:37 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "windows_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = var.subnet_id
+  key_name      = "new-key-pair"
 
   vpc_security_group_ids = [aws_security_group.windows_sg.id]  # ✅ Correct way
 
