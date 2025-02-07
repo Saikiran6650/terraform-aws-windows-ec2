be89b4a6599997f7f80a2fca4ef352e6978944ae
Saikiran6650 <saikiranpalcham09@gmail.com>
2025-02-07 10:41:26 -0500

    ec2 instance

---
+++ ec2/variables.tf
@@ -0,0 +1,18 @@
+variable "key_name" {
+  description = "The name of an existing AWS EC2 Key Pair to allow RDP access"
+  type        = string
+  default     = "new-key-pair"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "ami_id" {
+  description = "The Windows Server AMI to use"
+  type        = string
+  # Replace with the appropriate Windows Server AMI for your region
+  default     = "ami-0a0ebee827a585d06"
+}
